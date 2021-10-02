e5118599333c4bd75ce9a6d3a0b4fcee17666b7c
fidiwi <friedrichleowinkler@gmx.de>
2021-10-02 16:57:50 +0200

    reads/section plotting

--- plotFile.py
+++ plotFile.py
@@ -1,12 +1,24 @@
 import csv
+import numpy as np
 import matplotlib.pyplot as pyplot
 
-file = open('data.subsample/differnces_10000_2.0_90_0,.45000_.55000,1_pos.csv')
+file = open('data.subsample/Hirnet1_S1.subsample.csv')
 csvreader = csv.reader(file)
-rows = []
-next(csvreader)
+xAxis = np.arange(0, 1.01, 0.01)
+readsPerSection = [[] for _ in range(101)]
+
+#Daten auslesen und einordnen
 for row in csvreader:
-    rows.append([float(row[0])])
-print(rows)
-pyplot.plot(rows)
+    value = float(row[0])
+    index = int(value/0.01)
+    readsPerSection[index].append(value)
+
+#Daten umwandeln
+numberPerSection = [0] * 101
+for section in range(101):
+    numberPerSection[section] = len(readsPerSection[section])
+print(numberPerSection)
+pyplot.plot(xAxis, numberPerSection)
+pyplot.xlabel("Position")
+pyplot.ylabel("reads/section")
 pyplot.show()
