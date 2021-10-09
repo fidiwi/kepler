ff91d76cfc634bb3c7cad5a3861be4093ce2b1fb
fidiwi <friedrichleowinkler@gmx.de>
2021-10-09 18:17:53 +0200

    windows

--- plotFile.py
+++ plotFile.py
@@ -2,20 +2,22 @@ import csv
 import numpy as np
 import matplotlib.pyplot as pyplot
 
+windows = 1000
+
 file = open('data.subsample/Hirnet1_S1.subsample.csv')
 csvreader = csv.reader(file)
-xAxis = np.arange(0, 1.01, 0.01)
-readsPerSection = [[] for _ in range(101)]
+xAxis = np.arange(0, 1+1/windows, 1/windows)
+readsPerSection = [[] for _ in range(windows+1)]
 
 #Daten auslesen und einordnen
 for row in csvreader:
     value = float(row[0])
-    index = int(value/0.01)
+    index = int(value*windows)
     readsPerSection[index].append(value)
 
 #Daten umwandeln
-numberPerSection = [0] * 101
-for section in range(101):
+numberPerSection = [0] * (windows+1)
+for section in range(windows+1):
     numberPerSection[section] = len(readsPerSection[section])
 print(numberPerSection)
 pyplot.plot(xAxis, numberPerSection)
