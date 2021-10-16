bcc5f7a6c1540147b3da387518f3881d9725927e
fidiwi <friedrichleowinkler@gmx.de>
2021-10-16 12:45:02 +0200

    Degree-> radian

--- plotFile.py
+++ plotFile.py
@@ -5,15 +5,15 @@ import matplotlib.pyplot as pyplot
 
 windows = 50
 
-file = open('kepler\Probedaten\Hirnet_subsample\Hirnet7_S7.subsample.csv')
+file = open('Probedaten/Beispiesamples/Mail_lutz_3/5000/5000_3.0_0.0,0.0_pos.csv') #Probedaten/Beispiesamples/Mail_lutz_3/5000/5000_3.0_0.0,0.0_pos.csv
 csvreader = csv.reader(file)
-xAxisDiagram = np.arange(0, 1+1/windows, 1/windows)
-readsPerSection = [[] for _ in range(windows+1)]
+xAxisDiagram = np.arange(0, 1, 1/windows)
+readsPerSection = [[] for _ in range(windows)]
 datasetLength = 0
 
 #Daten für den Vektorenplot
-xPosSection = [[] for _ in range(windows+1)]
-yPosSection = [[] for _ in range(windows+1)]
+xPosSection = [[] for _ in range(windows)]
+yPosSection = [[] for _ in range(windows)]
 
 #Daten für den Kreisplot
 xList = [0]
@@ -22,11 +22,11 @@ yList = [0]
 #Daten auslesen und einordnen
 for row in csvreader:
     value = float(row[0])
-    index = int(round(value*windows, 0))
+    index = int(value*windows)
     readsPerSection[index].append(value)
     degree = value * 360
-    x = math.cos(degree)
-    y = math.sin(degree)
+    x = math.cos(math.radians(degree))
+    y = math.sin(math.radians(degree))
 
     xPosSection[index].append(x)
     yPosSection[index].append(y)
@@ -36,15 +36,16 @@ for row in csvreader:
 
     datasetLength+=1
 print(readsPerSection)
+print (xPosSection)
 #Daten umwandeln Graph
-numberPerSection = [0] * (windows+1)
+readAmountPerSection = [0] * (windows)
 
 #Positionen addieren Vektorplot
 xVectors = []
 yVectors = []
 
-for section in range(windows+1):
-    numberPerSection[section] = len(readsPerSection[section])
+for section in range(windows):
+    readAmountPerSection[section] = len(readsPerSection[section])
 
     vectorX = 0
     for x in xPosSection[section]:
@@ -58,18 +59,18 @@ for section in range(windows+1):
     erwartetY = math.sin((section/windows-(windows/2))*360)
     xVectors.append(vectorX/(datasetLength/windows))
     yVectors.append(vectorY/(datasetLength/windows))
-    print(erwartetX) 
-    print(erwartetY)
 
-print(numberPerSection)
-pyplot.plot(xAxisDiagram, numberPerSection)
+    print(vectorX)
+print(xVectors)
+    
+pyplot.plot(xAxisDiagram, readAmountPerSection)
 pyplot.xlabel("Position")
 pyplot.ylabel("reads/section")
 
 #Kreisplot erstellen
-#pyplot.figure()
-#pyplot.plot(xList, yList, '.', color='black')
-#pyplot.gca().set_aspect('equal', adjustable='box')
+pyplot.figure()
+pyplot.plot(xList, yList, '.', color='black')
+pyplot.gca().set_aspect('equal', adjustable='box')
 
 #Vektorplot erstellen
 pyplot.figure()
