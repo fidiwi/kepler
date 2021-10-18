e0667248b61d9e3fbcfa29b20ceab02c35d6280b
KiKoLEL <kianmars177@gmail.com>
2021-10-18 15:49:22 +0200

    graph 2

--- plotFile.py
+++ plotFile.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as pyplot
 
 windows = 100
 
-file = open('Probedaten/Beispiesamples/Mail_lutz_3/5000/5000_8.0_0.0,0.0_pos.csv') #Probedaten/Beispiesamples/Mail_lutz_3/5000/5000_3.0_0.0,0.0_pos.csv
+file = open('kepler/Probedaten/Beispiesamples/Mail_lutz_3/5000/5000_3.0_0.0,0.0_pos.csv') #Probedaten/Beispiesamples/Mail_lutz_3/5000/5000_3.0_0.0,0.0_pos.csv
 csvreader = csv.reader(file)
 xAxisDiagram = np.arange(0, 1, 1/windows)
 readsPerSection = [[] for _ in range(windows+1)] #Das +1 ist für den Fall des Wertes 1, der dann nicht mehr der größten Sektion zugeordnet werden kann.
@@ -23,7 +23,6 @@ yList = [0]
 for row in csvreader:
     value = float(row[0])
     index = int(value*windows)
-    print(index)
     readsPerSection[index].append(value)
     degree = value * 360
     x = math.cos(math.radians(degree))
@@ -36,14 +35,14 @@ for row in csvreader:
     yList.append(y)
 
     datasetLength+=1
-print(readsPerSection)
-print (xPosSection)
 #Daten umwandeln Graph
 readAmountPerSection = [0] * (windows)
 
 #Positionen addieren Vektorplot
 xVectors = []
 yVectors = []
+abstand = []
+vergleich1 = [1] * windows
 
 for section in range(windows):
     readAmountPerSection[section] = len(readsPerSection[section])
@@ -60,13 +59,18 @@ for section in range(windows):
     erwartetY = math.sin((section/windows-(windows/2))*360)
     xVectors.append(vectorX/(datasetLength/windows))
     yVectors.append(vectorY/(datasetLength/windows))
+    abstand.append(math.sqrt(xVectors[section]**2 + yVectors[section]**2))
 
-    print(vectorX)
-print(xVectors)
-    
+
 pyplot.plot(xAxisDiagram, readAmountPerSection)
 pyplot.xlabel("Position")
 pyplot.ylabel("reads/section")
+
+pyplot.figure()
+pyplot.plot(xAxisDiagram, abstand)
+pyplot.plot(xAxisDiagram, vergleich1, color='red')
+pyplot.xlabel("Position")
+pyplot.ylabel("Abstand zum Zentrum")
 
 #Kreisplot erstellen
 pyplot.figure()
