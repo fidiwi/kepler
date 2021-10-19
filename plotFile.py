828a5f798ec963ee72c1085423bf1cde76ee5cf5
fidiwi <friedrichleowinkler@gmx.de>
2021-10-19 15:53:51 +0200

    Schöner gemacht

--- plotFile.py
+++ plotFile.py
@@ -3,30 +3,39 @@ import math
 import numpy as np
 import matplotlib.pyplot as pyplot
 
-windows = 100
+def abstandZwPunkten(x1, y1, x2, y2):
+    diffX = abs(x1-x2)
+    diffY = abs(y1-y2)
 
-file = open('Probedaten/Hirnet_subsample/Hirnet4_S4.subsample.csv') #Probedaten/Beispiesamples/Mail_lutz_3/5000/5000_3.0_0.0,0.0_pos.csv
+    return(math.sqrt(diffX**2 + diffY**2))
+
+
+anzahlWindows = 100
+
+file = open('Probedaten/Beispiesamples/Mail_lutz_3/LБcken/20_percent/10000_2.0_20_.10000,.30000_pos.csv') #Probedaten/Beispiesamples/Mail_lutz_3/5000/5000_3.0_0.0,0.0_pos.csv
 csvreader = csv.reader(file)
-xAxisDiagram = np.arange(0, 1, 1/windows)
-readsPerSection = [[] for _ in range(windows+1)] #Das +1 ist für den Fall des Wertes 1, der dann nicht mehr der größten Sektion zugeordnet werden kann.
-datasetLength = 0
+xAxisDiagram = np.arange(0, 1, 1/anzahlWindows)
+readsPerSection = [[] for _ in range(anzahlWindows)] #Das +1 ist für den Fall des Wertes 1, der dann nicht mehr der größten Sektion zugeordnet werden kann
 
 #Daten für den Vektorenplot
-xPosSection = [[] for _ in range(windows+1)]
-yPosSection = [[] for _ in range(windows+1)]
+xPosSection = [[] for _ in range(anzahlWindows)]
+yPosSection = [[] for _ in range(anzahlWindows)]
 
 #Daten für den Kreisplot
 xList = [0]
 yList = [0]
 
+datasetLength = 0
 #Daten auslesen und einordnen
 for row in csvreader:
     value = float(row[0])
-    index = int(value*windows)
+    index = int(value*anzahlWindows)
+    if value == 1:
+        index = 0
     readsPerSection[index].append(value)
     degree = value * 360
-    x = math.cos(math.radians(degree))
-    y = math.sin(math.radians(degree))
+    x = math.cos(math.radians(degree)) # *1 weil r=1 bzw Betrag von Vektoren = 1
+    y = math.sin(math.radians(degree)) 
 
     xPosSection[index].append(x)
     yPosSection[index].append(y)
@@ -36,30 +45,28 @@ for row in csvreader:
 
     datasetLength+=1
 #Daten umwandeln Graph
-readAmountPerSection = [0] * (windows)
+readAmountPerSection = [0] * (anzahlWindows)
 
 #Positionen addieren Vektorplot
 xVectors = []
 yVectors = []
 abstand = []
-vergleich1 = [1] * windows
+vergleich1 = [1] * anzahlWindows
 
-for section in range(windows):
-    readAmountPerSection[section] = len(readsPerSection[section])
+for window in range(anzahlWindows):
+    readAmountPerSection[window] = len(readsPerSection[window])
 
     vectorX = 0
-    for x in xPosSection[section]:
+    for x in xPosSection[window]:
         vectorX+=x
     
     vectorY = 0
-    for y in yPosSection[section]:
+    for y in yPosSection[window]:
         vectorY+=y
 
-    erwartetX = math.cos((section/windows-(windows/2))*360)
-    erwartetY = math.sin((section/windows-(windows/2))*360)
-    xVectors.append(vectorX/(datasetLength/windows))
-    yVectors.append(vectorY/(datasetLength/windows))
-    abstand.append(math.sqrt(xVectors[section]**2 + yVectors[section]**2))
+    xVectors.append(vectorX/(datasetLength/anzahlWindows))
+    yVectors.append(vectorY/(datasetLength/anzahlWindows))
+    abstand.append(math.sqrt(xVectors[window]**2 + yVectors[window]**2))
 
 
 ketteY = [yVectors.pop(0)]
@@ -77,10 +84,8 @@ for yVector in ketteY:
         x2 = xVectors[yVectors.index(nextPossibleVector)]
         y2 = nextPossibleVector
 
-        diffX = abs(x1-x2)
-        diffY = abs(y1-y2)
+        abstandP = abstandZwPunkten(x1, y1, x2, y2)
 
-        abstandP = math.sqrt(diffX**2 + diffY**2)
         if not minAbstand or minAbstand > abstandP:
             minAbstand = abstandP
             pointWithMinAbstand = yVectors.index(nextPossibleVector)
