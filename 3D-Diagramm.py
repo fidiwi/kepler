40cb443d5491e14d92ebad2d2e82e151b5975126
KiKoLEL <kianmars177@gmail.com>
2021-10-27 15:04:38 +0200

    xVectors und yVectors

--- 3D-Diagramm.py
+++ 3D-Diagramm.py
@@ -1,7 +1,12 @@
 import testdatei
 
-anzahlWindows = 100
+anzahlWindows = 4
 fileName = 'kepler/Probedaten/Beispiesamples/Mail_lutz_3/5000/5000_3.0_0.0,0.0_pos.csv'
 
-VextorenListe = testdatei.__init__(fileName, anzahlWindows)
-print(VextorenListe)
+VectorenListe = testdatei.__init__(fileName, anzahlWindows)
+xVectors = VectorenListe[0]
+yVectors = VectorenListe[1]
+
+print(VectorenListe)
+print(xVectors)
+print(yVectors)
