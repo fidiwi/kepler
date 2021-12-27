770be2b7027bd6476d09aab8d661c19f219ecb91
enlui <enno.luitjens@protonmail.com>
2021-12-27 18:29:56 +0100

    Fachchinesisch + calcGrowthVector

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as pyplot
 import os
 
 # Eingaben: Dateiname, Abzahl Windows, Treshold
-filename= 'Probedaten/Beispiesamples/Mail_lutz_3/Luecken/20_percent/10000_4.0_20_.20000,.40000_pos.csv' #Probedaten/Beispiesamples/Mail_lutz_3/Luecken/20_percent/10000_2.0_20_.20000,.40000_pos.csv
+filename= 'Probedaten/Beispiesamples/Mail_lutz_3/Luecken/20_percent/10000_2.0_20_.60000,.80000_pos.csv' #Probedaten/Beispiesamples/Mail_lutz_3/Luecken/20_percent/10000_2.0_20_.20000,.40000_pos.csv
 readsPerWindow = 100 # Wieviele Reads in einem Window erwartet werden sollen, Windowanzahl passt sich der Datensatzgröße dynamisch an.
 thresholdLuecke = 1
 thresholdUeberschuss = -1
@@ -12,7 +12,7 @@ createFiles = True
 
 # Wachstumsdiagramme!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 if wachstumsdiagramme:
-    folderPosFiles = os.listdir("./Probedaten/Beispiesamples/Mail_lutz_3/testdateienLücken") # ./Probedaten/Beispiesamples/Mail_lutz_3/testdateienLücken || ./Probedaten/Beispiesamples/Mail_lutz_3/5000 Achtung: int(anzahlWindows/2)
+    folderPosFiles = os.listdir("./Probedaten/Beispiesamples/Mail_lutz_3/5000") # ./Probedaten/Beispiesamples/Mail_lutz_3/testdateienLücken || ./Probedaten/Beispiesamples/Mail_lutz_3/5000 Achtung: int(anzahlWindows/2)
     liste = []
     liste2 = []
     filledGapsListe = []
@@ -20,7 +20,7 @@ if wachstumsdiagramme:
     xValuesList = []
     yValuesList = []
     for file in folderPosFiles:
-        ultraClass = UltraClass("Probedaten/Beispiesamples/Mail_lutz_3/testdateienLücken/"+str(file), thresholdLuecke, thresholdUeberschuss, readsPerWindow)
+        ultraClass = UltraClass("Probedaten/Beispiesamples/Mail_lutz_3/5000/"+str(file), thresholdLuecke, thresholdUeberschuss, readsPerWindow)
         datasetList, readAmountPerSection, readAmountPerSectionDict, readsPerSectionDict, xVectors, yVectors, xAxisDiagram = ultraClass.readFile()
         degreeDiffList, xList, avg, standardAbw, relEaList = ultraClass.calcWinkel(datasetList)
         gapBereiche = ultraClass.determineGaps(relEaList, datasetList)
@@ -42,7 +42,9 @@ datasetList, readAmountPerSection, readAmountPerSectionDict, readsPerSectionDict
 degreeDiffList, xList, avg, standardAbw, relEaList = ultraClass.calcWinkel(datasetList)
 gapBereiche = ultraClass.determineGaps(relEaList, datasetList)
 linReg1, linReg2, filledGaps, filledEllipse = ultraClass.fillGaps(gapBereiche, readAmountPerSection, xAxisDiagram)
-windowAbwDict, betterDataFileName = ultraClass.getWindowAbweichung(filledGaps[0], filledGaps[1], readAmountPerSectionDict, readsPerSectionDict, createFiles, iteration=4)
+windowAbwDict, betterDataFileName = ultraClass.getWindowAbweichung(filledGaps[0], filledGaps[1], readAmountPerSectionDict, readsPerSectionDict, createFiles)
+#--------------------Wachstumsrate Enno--------------------
+ultraClass.calcGrowthVector(2, linReg1[1])
 #readAbweichungProWindow = ultraClass.windowQualität(readsPerSectionDict)
 
 
@@ -59,6 +61,7 @@ if createFiles:
 #print (windowAbwDict)
 #print (gapBereiche)
 
+#--------------------Plotting--------------------
 #Readamount pro Window, mit LinRegs
 pyplot.figure(num='Readamount pro Window')
 if wachstumsdiagramme:
